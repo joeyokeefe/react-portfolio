059aca623639d53fe86d1a23f1342aac8fdb3abf
joseph okeefe <joeyokeefe5@gmail.com>
2023-01-15 23:16:33 -0500

    styling changes focused on projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,25 @@ import "./Projects.css";
 function Projects() {
   return (
     <div id="projectsSection">
-      <h2 id="projectsHeader">My Dev Tools</h2>
+      <h2 id="projectsHeader">Projects</h2>
+      <div id="projectsContainer">
+        <div class="project">
+          <img class="projectThumbnail" src="" />
+          <h4 class="projectTitle">Endangerment!</h4>
+          <p class="projectDescription">
+            A trivia game made with jService API. Generates a random board of
+            Jeopardy questions to play with your friends.
+          </p>
+          <a
+            class="projectLink"
+            href="https://endangerment.netlify.app"
+            target="_blank"
+          >
+            Link
+          </a>
+        </div>
+      </div>
+      <h3 id="toolsHeader">My Dev Tools</h3>
       <div id="iconsContainer">
         <img src={htmlIcon} alt="html" class="softwareIcon" />
         <img src={cssIcon} alt="css" class="softwareIcon" />
