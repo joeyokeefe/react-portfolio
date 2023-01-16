23a1f3490d025d11ae0f8c740d19ae03786497e6
joseph okeefe <joeyokeefe5@gmail.com>
2023-01-15 21:48:00 -0500

    Styled resume page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,30 @@
+import cssIcon from "./icons/cssIcon.png";
+import htmlIcon from "./icons/htmlIcon.png";
+import javaScriptIcon from "./icons/javaScriptIcon.png";
+import reactIcon from "./icons/reactIcon.png";
+import nodeIcon from "./icons/nodeIcon.png";
+import bootstrapLogo from "./icons/bootstrapIcon.png";
+import tailwindIcon from "./icons/tailwindIcon.png";
+import githubLogo from "./icons/githubLogo.png";
+import vsCodeIcon from "./icons/vsCodeIcon.png";
+import "./Projects.css";
+
 function Projects() {
   return (
-    <div>
-      <h2>My Dev Tools</h2>
-      <img src="..." alt="html" />
-      <img src="..." alt="css" />
-      <img src="..." alt="javaScript" />
-      <img src="..." alt="mongoDB" />
-      <img src="..." alt="expressJS" />
-      <img src="..." alt="reactJS" />
-      <img src="..." alt="nodeJS" />
-      <img src="..." alt="SQL" />
-      <img src="..." alt="bootstrap" />
-      <img src="..." alt="github" />
-      <img src="..." alt="vscode" />
+    <div id="projectsSection">
+      <h2 id="projectsHeader">My Dev Tools</h2>
+      <div id="iconsContainer">
+        <img src={htmlIcon} alt="html" class="softwareIcon" />
+        <img src={cssIcon} alt="css" class="softwareIcon" />
+        <img src={javaScriptIcon} alt="javaScript" class="softwareIcon" />
+        <img src={reactIcon} alt="reactJS" class="softwareIcon" />
+        <img src={nodeIcon} alt="nodeJS" class="softwareIcon" />
+        <img src="..." alt="SQL" class="softwareIcon" />
+        <img src={bootstrapLogo} alt="bootstrap" class="softwareIcon" />
+        <img src={tailwindIcon} alt="tailwind" class="softwareIcon" />
+        <img src={githubLogo} alt="github" class="softwareIcon" />
+        <img src={vsCodeIcon} alt="vscode" class="softwareIcon" />
+      </div>
     </div>
   );
 }
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,51 +1,115 @@
+import "./Work.css";
+
 function Work() {
   return (
-    <div>
-      <h2>Work Experience</h2>
+    <div id="workSection">
+      <h2 id="workHeader">Work Experience</h2>
       {/*Job 1*/}
-      <h3>IT Administrator</h3>
-      <h4>St. Joseph Hill Academy</h4>
-      <h4>09/2019 - Present | Staten Island, NY</h4>
-      <ul>
-        <li>
-          Oversees the setup and configuration of new software and hardware.
-        </li>
-        <li>
-          Trains and coaches both students and peers in the use of technology
-          and digital resources including basic computer fluency, Google Docs,
-          Google Slides, Zoom, Zearn, Freckle, and more.
-        </li>
-        <li>Troubleshoots all IT and technology related problems.</li>
-        <li>Manages Google Admin and student database.</li>
-        <li>
-          Analyzes and interprets data to ensure student programs are effective
-          in meeting proposed goals
-        </li>
-        <li>
-          Oversees the coordination of disability services and early childhood
-          intervention.
-        </li>
-        <li>
-          Analyzes performance data to identify students who may require
-          additional services.
-        </li>
-        <li>
-          Assists in the design and creation of marketing and promotional
-          materials.
-        </li>
-        <li>
-          Oversees the scheduling and coordination of all inschool disability
-          service providers.
-        </li>
-      </ul>
+      <h3 class="jobHeader">IT Administrator</h3>
+      <h4 class="employer">St. Joseph Hill Academy</h4>
+      <h4 class="jobDetails">09/2019 - Present | Staten Island, NY</h4>
+      <div class="dutiesSection">
+        <ul>
+          <li>
+            Oversees the setup and configuration of new software and hardware.
+          </li>
+          <li>
+            Trains and coaches both students and peers in the use of technology
+            and digital resources including basic computer fluency, Google Docs,
+            Google Slides, Zoom, Zearn, Freckle, and more.
+          </li>
+          <li>Troubleshoots all IT and technology related problems.</li>
+          <li>Manages Google Admin and student database.</li>
+          <li>
+            Analyzes and interprets data to ensure student programs are
+            effective in meeting proposed goals
+          </li>
+          <li>
+            Oversees the coordination of disability services and early childhood
+            intervention.
+          </li>
+          <li>
+            Analyzes performance data to identify students who may require
+            additional services.
+          </li>
+          <li>
+            Assists in the design and creation of marketing and promotional
+            materials.
+          </li>
+          <li>
+            Oversees the scheduling and coordination of all inschool disability
+            service providers.
+          </li>
+        </ul>
+      </div>
       {/*Job 2*/}
-      <h3>Marketing Research Assistant</h3>
+      <h3 class="jobHeader">Marketing Research Assistant</h3>
+      <h4 class="employer">Love Law Firm, PLLC</h4>
+      <h4 class="jobDetails">11/2018 - 03/2019 | Uniondale, NY (Remote)</h4>
+      <div class="dutiesSection">
+        <ul>
+          <li>
+            Temporary contract position performing a variety of administrative,
+            operational, and marketing duties for a privately owned law firm.
+          </li>
+          <li>
+            Conducted market research to identify potential clients and primary
+            industry competitors.
+          </li>
+          <li>
+            Created databases of market information using Google Sheets and
+            Excel to ensure that information could be easily accessed and
+            edited.
+          </li>
+          <li>
+            Reported market research findings using digital data visualization
+            tools and Google Workspace apps to ensure that complex data could be
+            interpreted and used for a variety of purposes.
+          </li>
+        </ul>
+      </div>
 
       {/*Job 3*/}
-      <h3>Production Intern</h3>
-
+      <h3 class="jobHeader">Production Intern</h3>
+      <h4 class="employer">The Daily Show with Trevor Noah</h4>
+      <h4 class="jobDetails">01/2018 - 05/2018 | New York, NY</h4>
+      <div class="dutiesSection">
+        <ul>
+          <li>Prepared props, sets, and equipment for television shoots.</li>
+          <li>
+            Worked directly with an audience of over 200 people to promote a
+            positive live show experience and smooth operation.
+          </li>
+          <li>
+            Used programs such as Nesbit and Avid to log and transcribe video
+            data.
+          </li>
+          <li>
+            Helped maintain a clean, orderly, and efficient office through a
+            variety of clerical duties.
+          </li>
+        </ul>
+      </div>
       {/*Job 4*/}
-      <h3>Server, Bartender, Host</h3>
+      <h3 class="jobHeader">Server, Bartender, Host</h3>
+      <h4 class="employer">Arirang Hibachi Steakhouse</h4>
+      <h4 class="jobDetails">04/2016 - 05/2018 | Staten Island, NY</h4>
+      <div class="dutiesSection">
+        <ul>
+          <li>
+            Prioritized hospitality, communication, and customer service in a
+            fast-paced work environment.
+          </li>
+          <li>
+            Worked in multiple positions to cultivate a variety of skills.
+          </li>
+          <li>Assisted in the training of new employees.</li>
+          <li>
+            Ensured the restaurant environment was clean, welcoming, and
+            efficient.
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
